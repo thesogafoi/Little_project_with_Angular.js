42b64367aad263d91dc074864bf24a79b70a8d53
Alireza <lovelylektus@yahoo.com>
2017-12-09 00:47:46 -0800

    ANgular Created  + Home Component and Faq Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FaqComponent } from './faq/faq.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
